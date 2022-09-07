4249277d1abedde1fb9bf9e98c2e2d78ccec0432
Utku Çetinkaya <utku.cetinkaya@hotmail.com>
2022-09-07 03:22:02 +0300

    Initial Commit

---
+++ NewsProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsProject
+//
+//  Created by Utku Çetinkaya on 7.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
