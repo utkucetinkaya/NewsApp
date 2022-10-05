b979c4474b7cfe11b24eeed5ce312483b56d948d
Utku Çetinkaya <utku.cetinkaya@hotmail.com>
2022-10-05 19:12:56 +0300

    first commit

---
+++ NewsProject/Model/NewsModel.swift
@@ -0,0 +1,8 @@
+//
+//  NewsModel.swift
+//  NewsProject
+//
+//  Created by Utku Çetinkaya on 7.09.2022.
+//
+
+import Foundation
---
+++ NewsProject/Views/NewsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NewsTableViewCell.swift
+//  NewsProject
+//
+//  Created by Utku Çetinkaya on 7.09.2022.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
